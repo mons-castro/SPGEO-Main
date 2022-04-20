ae6322894150032880181a3ea17dcadabf7dc65a
mons-castro <61324266+mons-castro@users.noreply.github.com>
2022-04-20 13:02:55 -0500

    Add files via upload

---
+++ client/employees/createNewEmployee.php
@@ -0,0 +1,234 @@
+<div class="card mb-4">
+    <!-- Card header -->
+    <div class="card-header">
+        <h3 class="mb-0 text-uppercase">Registrar un nuevo empleado</h3>
+    </div>
+    <!-- Card body -->
+    <div class="card-body">
+        <h4 class="text-primary"><i class="fas fa-user"></i> Información Personal</h4>
+        <hr style="margin-top: 2px;">
+
+        <div class="row">
+            <div class="col-lg-3 col-md-3 col-sm-12 mb-3">
+                <div class="form-group">
+                    <label class="form-control-label" for="inputName">Nombre</label>
+                    <input type="text" class="form-control" id="inputName" placeholder="Ej. Karen">
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-12 mb-3">
+                <div class="form-group">
+                    <label class="form-control-label" for="inputSecondName">Segundo Nombre</label>
+                    <input type="text" class="form-control" id="inputSecondName" placeholder="Ej. Yunuen">
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-12 mb-3">
+                <div class="form-group">
+                    <label class="form-control-label" for="example3cols1Input">Apellido Paterno</label>
+                    <input type="text" class="form-control" id="example3cols1Input" placeholder="Ej. Mora">
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-12 mb-3">
+                <div class="form-group">
+                    <label class="form-control-label" for="example3cols1Input">Apellido Materno</label>
+                    <input type="text" class="form-control" id="example3cols1Input" placeholder="Ej. Quintana">
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-3 col-md-3 col-sm-12 mb-3">
+                <div class="form-group">
+                    <label class="form-control-label" for="example3cols1Input">Número de Teléfono</label>
+                    <input type="tel" class="form-control" id="example3cols1Input" placeholder="Ej. 3312991964">
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-12 mb-3">
+                <div class="form-group">
+                    <label class="form-control-label" for="example3cols1Input">Fecha de Nacimiento</label>
+                    <input class="form-control" type="date" value="0000-00-00" id="example-date-input">
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-12 mb-3">
+                <div class="form-group">
+                    <label class="form-control-label" for="exampleFormControlSelect1">Sexo</label>
+                    <select class="form-control" id="exampleFormControlSelect1">
+                        <option disabled selected>Selecciona el Género</option>
+                        <option value="M">Másculino</option>
+                        <option value="F">Femenino</option>
+                    </select>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-12 mb-3">
+                <div class="form-group">
+                    <label class="form-control-label" for="exampleFormControlSelect1">Religión</label>
+                    <select class="form-control" id="exampleFormControlSelect1">
+                        <option disabled selected>Selecciona la Religión</option>
+                        <option value="Hinduismo">Hinduismo</option>
+                        <option value="Judaísmo">Judaísmo</option>
+                        <option value="Taoísmo">Taoísmo</option>
+                        <option value="Budismo">Budismo</option>
+                        <option value="Cristianismo">Cristianismo</option>
+                        <option value="Jainismo">Jainismo</option>
+                        <option value="Sijismo">Sijismo</option>
+                        <option value="Islam">Islam</option>
+                        <option value="Bahaísmo">Bahaísmo</option>
+                        <option value="Catolicismo">Catolicismo</option>
+                        <option value="Satanismo">Satanismo</option>
+                    </select>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-3 col-md-3 col-sm-12 mb-3">
+                <div class="form-group">
+                    <label class="form-control-label" for="exampleFormControlSelect1">Estado Civil</label>
+                    <select class="form-control" id="exampleFormControlSelect1">
+                        <option disabled selected>Selecciona el Estado Civil</option>
+                        <option value="Soltero">Soltero</option>
+                        <option value="Casado">Casado</option>
+                        <option value="Divorciado">Divorciado</option>
+                        <option value="Separación en Proceso Judicial">Separación en Proceso Judicial</option>
+                        <option value="Viudo">Viudo</option>
+                        <option value="Concubinato">Concubinato</option>
+                        <option value="Relación Sentimental">Relación Sentimental</option>
+                    </select>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-12 mb-3">
+                <div class="form-group">
+                    <label class="form-control-label" for="inputName">Número de Seguro Social</label>
+                    <input type="text" class="form-control" id="inputName" placeholder="Ej. 69169823072">
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-12 mb-3">
+                <div class="form-group">
+                    <label class="form-control-label" for="inputName">RFC</label>
+                    <input type="text" class="form-control" id="inputName" placeholder="Ej. AAPA980929CC5">
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-12 mb-3">
+                <div class="form-group">
+                    <label class="form-control-label" for="inputName">CURP</label>
+                    <input type="text" class="form-control" id="inputName" placeholder="Ej. AAPA980929HMNYDB06">
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-3 col-md-3 col-sm-12 mb-3">
+                <div class="form-group">
+                    <label class="form-control-label" for="inputName">Clave de Elector</label>
+                    <input type="text" class="form-control" id="inputName" placeholder="Ej. AYPDAB98092916H900">
+                </div>
+            </div>
+        </div>
+
+
+
+        <h4 class="text-primary"><i class="fas fa-home"></i> Ubicación</h4>
+        <hr style="margin-top: 2px;">
+
+        <div class="row">
+            <div class="col-lg-6 col-md-6 col-sm-12 mb-3">
+                <div class="form-group">
+                    <label class="form-control-label" for="example3cols1Input">Código Postal</label>
+                    <input type="number" class="form-control" id="example3cols1Input" placeholder="Ej. 61100">
+                </div>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-12 mb-3 align-self-center">
+                <button type="button" class="btn btn-secondary"><i class="fas fa-search"></i> Buscar ubicación</button>
+            </div>
+        </div>
+
+        <section class="location">
+            <div class="row">
+                <div class="col-lg-4 col-md-4 col-sm-12 mb-3">
+                    <div class="form-group">
+                        <label class="form-control-label" for="example3cols1Input">Estado</label>
+                        <input type="number" class="form-control" id="example3cols1Input" placeholder="Este campo se llena autoáticamente">
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-12 mb-3">
+                    <div class="form-group">
+                        <label class="form-control-label" for="example3cols1Input">Municipio</label>
+                        <input type="number" class="form-control" id="example3cols1Input" placeholder="Este campo se llena autoáticamente">
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-12 mb-3">
+                    <div class="form-group">
+                        <label class="form-control-label" for="example3cols1Input">Colonia</label>
+                        <input type="number" class="form-control" id="example3cols1Input" placeholder="Este campo se llena autoáticamente">
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-6 col-md-6 col-sm-12 mb-3">
+                    <div class="form-group">
+                        <label class="form-control-label" for="example3cols1Input">Calle</label>
+                        <input type="number" class="form-control" id="example3cols1Input" placeholder="Este campo se llena autoáticamente">
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-12 mb-3">
+                    <div class="form-group">
+                        <label class="form-control-label" for="example3cols1Input">Número</label>
+                        <input type="number" class="form-control" id="example3cols1Input" placeholder="Este campo se llena autoáticamente">
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <h4 class="text-primary"><i class="fas fa-briefcase"></i> Información Laboral</h4>
+        <hr style="margin-top: 2px;">
+
+        <div class="row">
+            <div class="col-lg-6 col-md-6 col-sm-12 mb-3">
+                <div class="form-group">
+                    <label class="form-control-label" for="inputName">Licenciatura</label>
+                    <input type="text" class="form-control" id="inputName" placeholder="Ej. Ingeniería en Gestión Empresarial">
+                </div>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-12 mb-3">
+                <div class="form-group">
+                    <label class="form-control-label" for="exampleFormControlSelect1">Puesto de Trabajo</label>
+                    <select class="form-control" id="exampleFormControlSelect1">
+                        <option disabled selected>Selecciona el Puesto</option>
+                        <option>Desarrollador Web</option>
+                        <option>Desarrollador Móvil</option>
+                    </select>
+                </div>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-12">
+                <div class="form-group">
+                    <label class="form-control-label" for="exampleFormControlTextarea1">Biografía Laboral</label>
+                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="8"></textarea>
+                </div>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-12 align-self-center">
+                <label class="form-control-label" for="exampleFormControlTextarea1">Foto del Emmpleado</label>
+                <input type="file" class="filepond" name="filepond" accept="image/png, image/jpeg, image/jpg, image/gif" />
+            </div>
+        </div>
+
+        <h4 class="text-primary"><i class="fas fa-project-diagram"></i> Redes Sociales</h4>
+        <hr style="margin-top: 2px;">
+
+        <div class="row">
+            <div class="col-lg-4 col-md-4 col-sm-12 mb-3">
+                <div class="form-group">
+                    <label class="form-control-label" for="example3cols1Input">Nombre de Usuario de Facebook</label>
+                    <input type="text" class="form-control" id="example3cols1Input" placeholder="Ej. abrxy">
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-12 mb-3">
+                <div class="form-group">
+                    <label class="form-control-label" for="example3cols1Input">Nombre de Usuario de Instagram</label>
+                    <input type="text" class="form-control" id="example3cols1Input" placeholder="Ej. abe.dmg">
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-12 mb-3">
+                <div class="form-group">
+                    <label class="form-control-label" for="example3cols1Input">Nombre de Usuario de Linkedin</label>
+                    <input type="text" class="form-control" id="example3cols1Input" placeholder="Ej. abe_dmg">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ client/employees/employees.php
@@ -0,0 +1,44 @@
+<div class="row align-items-center">
+    <div class="col-lg-6 col-7">
+        <h6 class="h2 d-inline-block mb-0"><i class="fas fa-users text-info"></i> Empleados</h6>
+    </div>
+    <div class="col-lg-6 col-5 text-right">
+        <a href="?createNewEmployee" class="btn btn-neutral"><i class="fas fa-plus-circle"></i> Registrar Empleado</a>
+        <a href="?suspendedEmployees" class="btn btn-neutral"><i class="fas fa-trash"></i> Empleados Suspendidos</a>
+    </div>
+</div>
+
+<div class="row mt-3">
+    <div class="col-lg-3 col-md-3 col-sm-12 mb-3">
+        <div class="card-employee mb-2">
+            <div class="p-4">
+                <div class="d-flex justify-content-between">
+                    <div class="d-flex flex-row align-items-center">
+                        <div class="icon">
+                            <img src="assets/logos/arpan.svg" class="img-fluid" width="30" alt="">
+                        </div>
+                        <div class="ms-2 c-details ml-2">
+                            <h5 class="mb-0">Abraham Ayala Padilla</h5> <span>direccion@arpan.com.mx</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-5">
+                    <h3 class="heading">Director General y Desarrollador</h3>
+                    <div class="mt-5">
+                        <div class="progress">
+                            <div class="progress-bar" role="progressbar" style="width: 50%" aria-valuenow="50"
+                                 aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                        <div class="mt-3">
+                            <span class="text1">Peperoni's Pizza: <span class="text2"> 50% de avance</span></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card-footer mt-3 text-center">
+                <a href="#!" class=" text-muted">Más información</a>
+            </div>
+        </div>
+    </div>
+
+</div>
